227d6186ba65e9545f3ab3e0c8db2a8a92a30ad0
Aaron Wang <aaronw4932@gmail.com>
2026-07-12 01:12:45 -0400

    add listener interface to detect when card is tapped

---
+++ app/src/main/java/com/professional/b07legendaryproject2026/adapters/OnArtifactClickListener.java
@@ -0,0 +1,7 @@
+package com.professional.b07legendaryproject2026.adapters;
+
+import com.professional.b07legendaryproject2026.data.Artifact;
+
+public interface OnArtifactClickListener {
+    void onArtifactClick(Artifact artifact);
+}
