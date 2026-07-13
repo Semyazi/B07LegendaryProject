8f7176766cb177157304cf35bd3d373119ba4ee2
Kenny <kennyha2005@gmail.com>
2026-07-13 13:33:13 -0400

    feat: added id and username fields to class Comment along with corresponding setter and getter methods. Edited relevant constructors to include id and username.

--- app/src/main/java/com/professional/b07legendaryproject2026/data/Comment.java
+++ app/src/main/java/com/professional/b07legendaryproject2026/data/Comment.java
@@ -1,28 +1,38 @@
-package com.professional.b07legendaryproject2026;
+package com.professional.b07legendaryproject2026.data;
 
 import java.time.Instant;
 public class Comment {
+
+    private String id;
     private final String msg;
     private final Instant timestamp; // Timestamp in UTC for sorting
+    private String username;
 
-    public Comment(String msg) {
+    public Comment(String id, String msg, String username) {
+        this.id = id;
         this.msg = msg;
         this.timestamp = Instant.now();
-
+        this.username = username;
     }
 
-    public Comment(String msg, Instant timestamp) {
+    public Comment(String id, String msg, Instant timestamp, String username) {
+        this.id = id;
         this.msg = msg;
         this.timestamp = timestamp;
-
+        this.username = username;
     }
 
     //Get methods
+    public String getId() { return id;}
     public String getMsg() {
         return msg;
     }
-
     public Instant getTimestamp() {
         return timestamp;
     }
+    public String getUsername() { return username; }
+
+    //Set methods
+    public void setUsername(String username) { this.username = username;}
 }
+
